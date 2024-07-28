880929e717b5bed135ea945830d89422699db61c
Yurii Holubchenko <yurii.holubchenko@gmail.com>
2024-07-28 16:13:32 +0200

    2235. Add two integers.

---
+++ 2235.add-two-integers.go
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/add-two-integers/
+
+// Test cases:
+// num1 = 12, num2 = 5
+// num1 = -10, num2 = 4
+
+// Expected results:
+// 17
+// -6
+
+func sum(num1 int, num2 int) int {
+  return num1 + num2
+}
